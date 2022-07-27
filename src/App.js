07e537a3558eb2e856dd29ab43e000e443538cdb
Mikelangelo64 <donni2001331gmail.com>
2022-07-28 00:22:16 +0300

    adding Typography component v-1.2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import './App.css';
-import './resourses/styles/style.scss'
+import styleVars from './resourses/styles/style.scss'
 import Typography from "./components/Typography";
 
 function App() {
+
   return (
     <div className="App">
         Immerse yourself in the army life
         <Typography titleNum='h1'>h1</Typography>
-        <Typography titleNum='h1' isYellow={true}>h1 yellow</Typography>
-        <Typography titleNum='h2'>h2</Typography>
-        <Typography titleNum='h2' h2Spec='collect'> <span>h2</span> collect</Typography>
-        <Typography titleNum='h2' h2Spec='immerse'><span>h2</span> immerse</Typography>
-        <Typography titleNum='h3'>h3</Typography>
-        <Typography titleNum='h3' isH3Card={true}>h3 card</Typography>
+        <Typography titleNum='h1' >h1 <span>yellow</span></Typography>
+        <Typography titleNum='h2' >h2</Typography>
+        <Typography titleNum='h3' color='secondary'><span>h3</span> immerse</Typography>
+        <Typography titleNum='h3' color='primary'>h3</Typography>
+        <Typography titleNum='h3' color='primary'> h3</Typography>
+        <Typography titleNum='h4' >h4</Typography>
     </div>
   );
 }
--- src/components/Typography/index.jsx
+++ src/components/Typography/index.jsx
@@ -1,47 +1,49 @@
 import React from 'react';
+import styleVars from '../../resourses/styles/style.scss'
 import stylesTypography from './Typography.module.scss'
-import stylesH2Collect from './H2Collect.module.scss'
-import stylesH2Immerse from './H2Immerse.module.scss'
-import stylesH3Card from './H3Card.module.scss'
-import stylesTitleYellow from './TitleYellow.module.scss'
 
 
-const Typography = ({children, titleNum, isYellow=false, h2Spec='', isH3Card=false}) => {
+const Typography = ({children, titleNum, color= 'primary' }) => {
+
     if(titleNum === 'h1'){
         return (
             <h1
-                className={`${stylesTypography.h1} ${isYellow ? stylesTitleYellow['title-yellow'] : ''}`}
+                className={`${stylesTypography.h1}`}
             >
                 {children}
             </h1>
         );
     }
     if(titleNum === 'h2'){
-        let specH2Name = ''
-
-        if(h2Spec === 'collect') {
-            specH2Name = stylesH2Collect['h2-spec']
-        }
-        if(h2Spec === 'immerse') {
-            specH2Name = stylesH2Immerse['h2-spec']
-        }
 
         return (
             <h2
-                className={`${stylesTypography.h2} ${specH2Name}`}
+                className={`${stylesTypography.h2}`}
             >
                 {children}
             </h2>
         );
     }
+
     if(titleNum === 'h3'){
+
         return (
             <h3
-                className={`${stylesTypography.h3} ${isH3Card ? stylesH3Card['h3-card'] : ''}`}
+                className={`${stylesTypography.h3} ${color === 'secondary' && stylesTypography['h3-secondary']}`}
             >
                 {children}
             </h3>
         );
+    }
+    if(titleNum === 'h4'){
+
+        return(
+            <h4
+                className={`${stylesTypography.h4}`}
+            >
+                {children}
+            </h4>
+        )
     }
 };
 
