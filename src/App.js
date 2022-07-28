9770b31e8ddc25e3ab6f737de0711867b500ea07
Mikelangelo64 <donni2001331gmail.com>
2022-07-28 21:22:27 +0300

    adding Image component v-1.2

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import './App.css';
 import './resourses/styles/style.scss'
 import Image from "./components/Image";
 
-import imgMok from './resourses/moks/chevron-1.png'
+import imgMock from './resourses/mocks/chevron-1.png'
 
 function App() {
 
   return (
     <div className="App">
         Immerse yourself in the army life
-        <Image src={imgMok} pictureComponent='drop' alt='drop-img'/>
-        <Image src={imgMok} pictureComponent='buy' alt='buy-img'/>
-        <Image src={imgMok} pictureComponent='rank' alt='rank-img'/>
-        <Image src={imgMok} pictureComponent='control' alt='control-img'/>
+        <Image src={imgMock} size='img-sm' alt='control-img'/>
+        <Image src={imgMock} size='img-md' alt='rank-img'/>
+        <Image src={imgMock} size='img-lg' alt='drop-img'/>
+        <Image src={imgMock} size='img-xl' alt='buy-img'/>
     </div>
   );
 }
--- src/components/Image/index.jsx
+++ src/components/Image/index.jsx
@@ -1,29 +1,11 @@
 import React from 'react';
 import styles from './Img.module.scss'
-import stylesDrop from './ImgDrop.module.scss'
-import stylesBuy from './ImgBuy.module.scss'
-import stylesRank from './ImgRank.module.scss'
-import stylesControl from './ImgControl.module.scss'
 
-const Image = ({src, pictureComponent, alt}) => {
-    let additionalClass = ''
-
-    if(pictureComponent === 'drop'){
-        additionalClass = stylesDrop['img-container']
-    }
-    if(pictureComponent === 'buy'){
-        additionalClass = stylesBuy['img-container']
-    }
-    if(pictureComponent === 'rank'){
-        additionalClass = stylesRank['img-container']
-    }
-    if(pictureComponent === 'control'){
-        additionalClass = stylesControl['img-container']
-    }
+const Image = ({src, size='img-md', alt}) => {
 
     return (
         <div
-            className={`${styles['img-container']} ${additionalClass}`}
+            className={`${styles['img-container']} ${styles[size]}`}
         >
             <img src={src} alt={alt}/>
         </div>
