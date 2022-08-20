157050ed01beee5a5de3939006e6f2b800bd217f
Сергей <sergejjolejj@gmail.com>
2022-08-20 16:29:49 +0300

    join community section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,30 @@
 import React from 'react';
-import Link from "../components/Link";
-import Image from "../components/Image";
-import imgMock from "../resourses/mocks/chevron-1.png";
-import Typography from "../components/Typography";
-import Button from "../components/Button";
+import Community from "../components/Community";
+// import Link from "../components/Link";
+// import Image from "../components/Image";
+// import imgMock from "../resourses/mocks/chevron-1.png";
+// import Typography from "../components/Typography";
+// import Button from "../components/Button";
 
 const Home = () => {
   return (
     <>
-      <Link href='#' isBtn={false}>OpenSea</Link>
-      <Link href='#' isBtn={true}>Sign in</Link>
-      <Image src={imgMock} size='img-sm' alt='control-img'/>
-      <Image src={imgMock} size='img-md' alt='rank-img'/>
-      <Image src={imgMock} size='img-lg' alt='drop-img'/>
-      <Image src={imgMock} size='img-xl' alt='buy-img'/>
-      <Typography titleNum='h1'>h1</Typography>
-      <Typography titleNum='h1' >h1 <span>yellow</span></Typography>
-      <Typography titleNum='h2' >h2</Typography>
-      <Typography titleNum='h3' color='secondary'><span>h3</span> immerse</Typography>
-      <Typography titleNum='h3' color='primary'>h3</Typography>
-      <Typography titleNum='h3' color='primary'> h3</Typography>
-      <Typography titleNum='h4' >h4</Typography>
-      <Button isYellow={true}>hui</Button>
-      <Button isYellow={false}>hui</Button>
+        <Community />
+      {/*<Link href='#' isBtn={false}>OpenSea</Link>*/}
+      {/*<Link href='#' isBtn={true}>Sign in</Link>*/}
+      {/*<Image src={imgMock} size='img-sm' alt='control-img'/>*/}
+      {/*<Image src={imgMock} size='img-md' alt='rank-img'/>*/}
+      {/*<Image src={imgMock} size='img-lg' alt='drop-img'/>*/}
+      {/*<Image src={imgMock} size='img-xl' alt='buy-img'/>*/}
+      {/*<Typography titleNum='h1'>h1</Typography>*/}
+      {/*<Typography titleNum='h1' >h1 <span>yellow</span></Typography>*/}
+      {/*<Typography titleNum='h2' >h2</Typography>*/}
+      {/*<Typography titleNum='h3' color='secondary'><span>h3</span> immerse</Typography>*/}
+      {/*<Typography titleNum='h3' color='primary'>h3</Typography>*/}
+      {/*<Typography titleNum='h3' color='primary'> h3</Typography>*/}
+      {/*<Typography titleNum='h4' >h4</Typography>*/}
+      {/*<Button isYellow={true}>hui</Button>*/}
+      {/*<Button isYellow={false}>hui</Button>*/}
     </>
   )
 }
