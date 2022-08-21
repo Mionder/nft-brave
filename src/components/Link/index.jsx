9b57ee7867eaf97e587c3fb2501beabd275d90e0
Mikelangelo64 <donni2001331gmail.com>
2022-08-21 23:03:14 +0300

    added Footer, modifying component Link v-0.1

--- src/components/Link/index.jsx
+++ src/components/Link/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import styles from './Link.module.scss'
 
-const Link = ({children, isBtn, href}) => {
+const Link = ({children, isBtn, href, className}) => {
     return (
         <a
             href={href}
-            className={`${styles.link} ${isBtn && styles['like-btn']}`}
+            className={`${styles.link} ${isBtn && styles['like-btn']} ${className}`}
         >{children}</a>
     );
 };
