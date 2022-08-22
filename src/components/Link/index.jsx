81dac2f36c7e5519d442de67f35cb5c9599ec831
Mikelangelo64 <donni2001331gmail.com>
2022-08-22 23:37:27 +0300

    change tags <a> to component Link, added real social, creators hrefs v-0.2

--- src/components/Link/index.jsx
+++ src/components/Link/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from './Link.module.scss'
 
-const Link = ({children, isBtn, href, className}) => {
+const Link = ({children, isBtn = false, href, className, target = '_blank'}) => {
     return (
         <a
             href={href}
             className={`${styles.link} ${isBtn && styles['like-btn']} ${className}`}
+            target={target}
         >{children}</a>
     );
 };
