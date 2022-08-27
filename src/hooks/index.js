2094a9007072f615a098c554dcaf92bf6440dc8b
Mikelangelo64 <donni2001331gmail.com>
2022-08-27 23:39:42 +0300

    added instrument for adding Chevrons in database, also create element SelectCustom(because, without decompose logic in mui Select, React betrayed scary error "unknown types blablabla"), component without normal styles, looks damp v-0.1

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -8,6 +8,18 @@ const getBrigades = async () => {
     });
 }
 
+const getChevrons = async () => {
+    return await fetch('http://localhost:3000/nft')
+        .then((response) => {
+            console.log(1, response)
+            return response.json()
+        })
+        .then((data) => {
+            console.log(2, data)
+            return data
+        })
+}
+
 const emptyObjectBrigade = {
   id: Math.random(),
   name: '',
@@ -22,5 +34,14 @@ const emptyObjectBrigade = {
   videoLink: '',
   img: '',
 }
+const emptyObjectChevron = {
+  id: Math.random(),
+  description: '',
+  editions: 1000,
+  name: '',
+  price: 30,
+  video: '',
+  brigadeId: '',
+}
 
-export { getBrigades, emptyObjectBrigade };
+export { getBrigades, emptyObjectBrigade, getChevrons, emptyObjectChevron};
